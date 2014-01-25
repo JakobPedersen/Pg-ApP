ed873d681b77abc1ccf4693cf6406fe0c353f52e
Jakob Pedersen <jakob@devoso.com>
2014-01-25 02:37:28 +0100

    added firebase

--- build.config.js
+++ build.config.js
@@ -62,11 +62,15 @@ module.exports = {
    */
   vendor_files: {
     js: [
+      'vendor/jquery/jquery.min.js',
       'vendor/angular/angular.js',
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
-      'vendor/angular-ui-utils/modules/route/route.js'
+      'vendor/angular-ui-utils/modules/route/route.js',
+      'vendor/firebase/firebase.js',
+      'vendor/firebase-simple-login/firebase-simple-login.js',
+      'vendor/angularfire/angularfire.min.js'
     ],
     css: [
     ],
--- src/app/app.js
+++ src/app/app.js
@@ -2,20 +2,26 @@ angular.module( 'Pg-ApP', [
   'templates-app',
   'templates-common',
   'Pg-ApP.home',
-  'ui.router'
-])
+  'Pg-ApP.login',
+  'ui.router',
+  'firebase'
+  ])
 
 .config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
   $urlRouterProvider.otherwise( '/home' );
 })
 
-.run( function run () {
+.run( function($rootScope, $location, AuthenticationSvc) { 
 })
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location, $firebase, $firebaseSimpleLogin ) {  
+  var ref = new Firebase('https://pg-app.firebaseio.com');
+  $scope.auth = $firebaseSimpleLogin(ref);
+  
+
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
       $scope.pageTitle = toState.data.pageTitle + ' | pg-app' ;
     }
-  });
+  });  
 });
